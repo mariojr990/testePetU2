5a67a9bb692a4f3c5b341e1d73b4f88ef81f4ff9
NiwZera <jcdjnilton196@gmail.com>
2020-05-13 00:27:48 -0300

    Initial commit

---
+++ app/src/main/java/com/pet2u/pet2u/CadPetActivity.java
@@ -0,0 +1,14 @@
+package com.pet2u.pet2u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CadPetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cad_pet);
+    }
+}
