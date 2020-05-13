2fe8af756749329706fe7e3f9f6c63e601045c7c
Willian Gonçalves Silva <willian2105@gmail.com>
2020-05-13 02:39:51 -0300

    Telas de Login e Splash Activity

---
+++ app/src/main/java/com/pet2u/pet2u/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.pet2u.pet2u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                abrirLogin();
+            }
+        }, 3000);
+
+    }
+
+    private void abrirLogin(){
+        Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
