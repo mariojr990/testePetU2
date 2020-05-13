23bf6f15d70642c2557936d3bf544231b6181698
NiwZera <jcdjnilton196@gmail.com>
2020-05-13 02:50:40 -0300

    Correção de bugs do merge

--- app/src/main/java/com/pet2u/pet2u/MainActivity.java
+++ app/src/main/java/com/pet2u/pet2u/MainActivity.java
@@ -11,9 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-<<<<<<< HEAD
-
-=======
->>>>>>> db58a25ff3d1a02de4c463fb565550ae7a4c4108
     }
 }
