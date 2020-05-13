db58a25ff3d1a02de4c463fb565550ae7a4c4108
NiwZera <jcdjnilton196@gmail.com>
2020-05-13 02:18:16 -0300

    Telas de cadastro de usuário e Petshop e tela de perfil do usuário

--- app/src/main/java/com/pet2u/pet2u/CadPetActivity.java
+++ app/src/main/java/com/pet2u/pet2u/CadPetActivity.java
@@ -10,5 +10,6 @@ public class CadPetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cad_pet);
+        getSupportActionBar().hide();
     }
 }
