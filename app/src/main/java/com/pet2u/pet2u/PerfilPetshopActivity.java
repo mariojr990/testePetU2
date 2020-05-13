7a4eac7d220c36d032e947d0f9c26097d8312044
NiwZera <jcdjnilton196@gmail.com>
2020-05-13 13:48:45 -0300

    Criação da página de listagem de pet e de PerfilPetshop

---
+++ app/src/main/java/com/pet2u/pet2u/PerfilPetshopActivity.java
@@ -0,0 +1,14 @@
+package com.pet2u.pet2u;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PerfilPetshopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_petshop);
+    }
+}
