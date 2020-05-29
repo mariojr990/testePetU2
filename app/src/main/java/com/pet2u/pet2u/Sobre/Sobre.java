ad884c562e8bc421f62f4c4b4ab210b3655b3854
unknown <felipecgalvao@gmail.com>
2020-05-28 23:14:16 -0300

    página sobre

---
+++ app/src/main/java/com/pet2u/pet2u/Sobre/Sobre.java
@@ -0,0 +1,16 @@
+package com.pet2u.pet2u.Sobre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pet2u.pet2u.R;
+
+public class Sobre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+    }
+}
