4fc1e82ddc7e7e36fa229e0e08c1a49d0e36de34
Josenilton Cezar <jcdjnilton196@gmail.com>
2020-06-17 15:24:28 -0300

    1) Alteração no layout do perfil do usuário.

--- app/src/main/java/com/pet2u/pet2u/Usuario/PerfilUsuario_EditarPefil.java
+++ app/src/main/java/com/pet2u/pet2u/Usuario/PerfilUsuario_EditarPefil.java
@@ -31,21 +31,21 @@ import com.pet2u.pet2u.Helper.Criptografia;
 import com.pet2u.pet2u.Helper.Permissao;
 import com.pet2u.pet2u.Login.MainActivity;
 import com.pet2u.pet2u.Petshop.ListagemPetshop_Activity;
+import com.pet2u.pet2u.Petshop.PerfilPet_Usuario;
 import com.pet2u.pet2u.R;
 import com.pet2u.pet2u.Sobre.Sobre;
 import com.pet2u.pet2u.modelo.Usuario;
 import com.squareup.picasso.Picasso;
 
-public class PerfilUsuario_Activity extends AppCompatActivity {
-
+public class PerfilUsuario_EditarPefil extends AppCompatActivity {
 
     private final static int CODIGO_SELECAO_FOTO = 1;
     public String [] permissoesNecessarias = new String[]{
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
 
-    private TextView nomeCompleto, telefone, email;
-    private Button botao_petshops_list;
+    private TextView nomeCompleto, telefone;
+    private Button botao_alterarSenha, botao_alterarTelefone, botao_alterarNome, botao_petshops_list, voltar_editarpefilusuario;
     private ImageButton fotoPerfilUsuario;
 
     private FirebaseAuth auth;
@@ -56,13 +56,14 @@ public class PerfilUsuario_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_perfil_usuario);
+        setContentView(R.layout.activity_perfil_usuario__editar_pefil);
         Permissao.validarPermissoes(permissoesNecessarias, this,1);
         getSupportActionBar().hide();
         inicializaComponentes();
         verificaUser();
         eventoClick();
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -102,10 +103,40 @@ public class PerfilUsuario_Activity extends AppCompatActivity {
     }
 
     private void eventoClick() {
+
+        botao_alterarSenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), EsqueceuSenha_Activity.class));
+            }
+        });
+
+        botao_alterarTelefone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), EditarTelefone_aActivity.class));
+            }
+        });
+
+        botao_alterarNome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), EditarNomeActivity.class));
+            }
+        });
+
         botao_petshops_list.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(), ListagemPetshop_Activity.class));
+            }
+        });
+
+        voltar_editarpefilusuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), PerfilUsuario_Activity.class));
+                finish();
             }
         });
         fotoPerfilUsuario.setOnClickListener(new View.OnClickListener() {
@@ -118,43 +149,10 @@ public class PerfilUsuario_Activity extends AppCompatActivity {
         });
     }
 
-
-
-    public void ClickListagemPetshop(View view){
-        startActivity(new Intent(getApplicationContext(), ListagemPetshop_Activity.class));
-    }
-    public void clickLogoutUsuario(View view){
-        Conexao.logOut();
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
-    }
-    public void clickSobre(View view){
-        startActivity(new Intent(getApplicationContext(), Sobre.class));
-    }
-
-    public void clickEditarPerfil(View view){
-        startActivity(new Intent(getApplicationContext(), PerfilUsuario_EditarPefil.class));
-        finish();
-    }
-
     private void verificaUser() {
         if(user == null){
             finish();
         }else{
-            String idUsuarioFoto = Criptografia.codificar(user.getEmail());
-            storageReference.child("FotoPerfilUsuario/" + idUsuarioFoto).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                @Override
-                public void onSuccess(Uri uri) {
-                    Picasso.get().load(uri).fit().centerInside().into(fotoPerfilUsuario);
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Log.d("xesque", " A imagem não existe");
-                }
-            }) ;
-
             //CRIA A LIGAÇÃO ENTRE O USUÁRIO LOGADO E O DATABASE DELE
             String emailUsuario = user.getEmail();
             String idUsuario = Criptografia.codificar(emailUsuario);
@@ -164,9 +162,21 @@ public class PerfilUsuario_Activity extends AppCompatActivity {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                     Usuario usuario = dataSnapshot.getValue(Usuario.class);
+                    String idUsuarioFoto = Criptografia.codificar(user.getEmail());
+
+                    storageReference.child("FotoPerfilUsuario/" + idUsuarioFoto).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            Picasso.get().load(uri).fit().centerInside().into(fotoPerfilUsuario);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d("xesque", " A imagem não existe");
+                        }
+                    }) ;
 
                     nomeCompleto.setText(usuario.getNome());
-                    email.setText(usuario.getEmail());
                     telefone.setText(usuario.getTelefone());
                 }
 
@@ -179,12 +189,16 @@ public class PerfilUsuario_Activity extends AppCompatActivity {
         }
     }
 
+
     private void inicializaComponentes(){
-        nomeCompleto = findViewById(R.id.nomeCompletoUsuario);
-        telefone = findViewById(R.id.editarTelefone);
-        email = findViewById(R.id.editarEmail);
-        botao_petshops_list = findViewById(R.id.botaoIrpara_listagemPetshop_perfilUsu);
-        fotoPerfilUsuario = findViewById(R.id.botaoSelecionarFotoPerfil);
+        nomeCompleto = findViewById(R.id.nomeCompletoUsuario2);
+        telefone = findViewById(R.id.editarTelefone2);
+        botao_alterarSenha = findViewById(R.id.botaoAlterarSenha2);
+        botao_alterarTelefone = findViewById(R.id.botaoAlterarTelefone2);
+        botao_alterarNome= findViewById(R.id.botaoAlterarNome2);
+        botao_petshops_list = findViewById(R.id.botaoIrpara_listagemPetshop_perfilUsu2);
+        fotoPerfilUsuario = findViewById(R.id.botaoSelecionarFotoPerfil2);
+        voltar_editarpefilusuario = findViewById(R.id.voltar_editarpefilusuario);
 
         auth = Conexao.getFirebaseAuth();
         databaseReference = Conexao.getFirebaseDatabase();
@@ -192,5 +206,4 @@ public class PerfilUsuario_Activity extends AppCompatActivity {
         user = auth.getCurrentUser();
 
     }
-
 }
